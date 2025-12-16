5305b16a02ab1b5548f4349f30deac90bbdb1b4e
Jazz0628 <jairamjk2009@gmail.com>
2025-12-16 21:55:17 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -1,8 +1,8 @@
 import motor.motor_asyncio
 from umongo import Instance, Document, fields
-# --- THE NECESSARY CHANGE IS HERE ---
-from umongo.frameworks.motor_asyncio import MotorAsyncIO 
-# ------------------------------------
+# --- CRITICAL FIX: IMPORT THE MODULE, NOT THE CLASS DIRECTLY ---
+import umongo.frameworks.motor_asyncio as umongo_motor
+# ---------------------------------------------------------------
 import datetime
 from info import DATABASE_NAME, COLLECTION_NAME
 import logging
@@ -16,7 +16,6 @@ class DummyClient:
     def __init__(self):
         self.db = None 
 
-# The placeholder object is created
 client = DummyClient()
 
 def get_db_instance():
@@ -32,7 +31,9 @@ async def initialize_umongo(motor_client: motor.motor_asyncio.AsyncIOMotorClient
     client.db = motor_client[DATABASE_NAME] 
     
     global umongo_instance
-    umongo_instance = Instance(MotorAsyncIO(client.db))
+    # --- USE THE IMPORTED MODULE REFERENCE TO ACCESS THE CLASS ---
+    umongo_instance = Instance(umongo_motor.MotorAsyncIO(client.db))
+    # -------------------------------------------------------------
     
     logger.info(f"Umongo Instance initialized for DB: {DATABASE_NAME}")
 
@@ -70,6 +71,5 @@ class Media(Document):
 
 class Media(Media):
     class Meta:
-        # This line will use the get_db_instance function to find the collection
         collection = get_db_instance()[COLLECTION_NAME] 
         pass
